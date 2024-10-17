edd56972fdc44caa7a69e0c1ad61bc9e1c5665a3
unknown <mb124969@gmail.com>
2024-10-17 22:42:11 +0530

    first commit

---
+++ dom/js/dscript.js
@@ -0,0 +1,9 @@
+
+
+// let elm=document.getElementById("demo");
+// elm.innerHTML="<h1>maneesh</h1>";
+// let fsc=document.getElementsByClassName("fs");
+// //console.log(fsc.length)
+// for(var i=0;i<fsc.length;i++){
+//     fsc[i].innerHTML="<h2>mk budholiya </h2>";
+// }
---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+
+let str=" this is javascript tutorial ,my name is maneesh good for you. ";
+
+// let st =str.substring(8,29)
+// document.write(st)
+// let position=str.indexOf("good");// str.lastIndexOf("good"),str.indexOf("is",position)
+// alert(position)
+// let trimedStr=str.trim();//str.trimStart(); str.trimEnd();
+// let rplace=str.replace("good","best")
+// console.log(str);
+// console.log(rplace)
+// let h=str.includes("html");
+// console.log(h);
+// let arr=["math",'physics','english','science','bio','computer science'];
+// console.log(arr);
+let book=new Array("math",'physics','english','science','bio','computer science')
+// book="rahul";
+// book[7]="sakshi"
+// book.push("monoj");
+// book.push("gobardhan");
+// book.pop();
+//console.log(book.length);
+// book.unshift("rosan");
+// book.shift()
+//book.splice(2,3,"khushi")
+// let position=book.indexOf('science')
+//  console.log( position);
+// let txt="this is a random text";
+// let arr=txt.split(" ");
+// //let arr=txt.split("t");
+
+// console.log(arr)
+// console.log(Array.isArray(arr));
+// let book2=["rk","dk"];
+// let book3=['pk',"nk"];
+
+// let mrgArr=book.concat(book2,book3);
+//  let bookStr=book.join(" ")
+//console.log(mrgArr);
+
+//  book.forEach((val)=>{
+//     console.log(`eliment ${val}`)
+//  })
+
+// let x=Math.floor(4.5);
+// let y=Math.ceil(6.4)
+// let z=Math.round(4.6)
+// let w=Math.trunc(5.1);
+// let rn=Math.floor(Math.random()*10+1);
+// console.log(rn)
+
+// function rno(min,max){
+//    // let y=Math.floor(Math.random()*(max-min)+min)
+//     let y=Math.floor(Math.random()*(max-min+1)+min)
+
+//    return y;
+// }
+// console.log(rno(24,15))
+// let dt=new Date();
+// document.write(dt)
+
+// let persion=new Object()
+
+// persion={
+//    name:"maneesh",
+//    age:26,
+//  get getname(){
+//      return this.name.toUpperCase();
+//    }
+// }
+// console.log(persion.getname)
