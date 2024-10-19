a691d862234cd6a1b63f61d93175d1ad6a1ac9fb
unknown <mb124969@gmail.com>
2024-10-19 21:33:17 +0530

    second commit

--- dom/js/dscript.js
+++ dom/js/dscript.js
@@ -7,3 +7,22 @@
 // for(var i=0;i<fsc.length;i++){
 //     fsc[i].innerHTML="<h2>mk budholiya </h2>";
 // }
+
+// let elm=document.getElementById("qry");
+//   let peragraph=elm.querySelectorAll("h2");
+//   console.log(peragraph)
+//  for(var i=0;i<peragraph.length;i++){
+//     peragraph[i].textContent="mk";
+//  }
+// let elm=document.getElementById("sl");
+// // let perent=elm.parentElement;
+
+// // console.log(perent);
+// let list1=document.createElement("li");
+// list1.textContent="list 4";
+// let pr=elm.parentElement;
+// let addli=elm.nextElementSibling.nextElementSibling;
+//      addli.appendChild(list1)
+// let clone=pr.cloneNode(true);
+// document.body.appendChild(clone);
+// console.log(list1)
